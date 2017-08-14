6e7670f02d92708b2de79e27b9c7c0c9865aa4d4
Kito Joseph <kitojoseph@gmail.com>
2017-08-14 11:36:28 -0400

    Added some networking stuff in using InetAddress class.

--- JavaNetworking/src/javanetworking/JavaNetworking.java
+++ JavaNetworking/src/javanetworking/JavaNetworking.java
@@ -8,6 +8,9 @@ https://docs.oracle.com/javase/tutorial/networking/urls/readingWriting.html
  */
 package javanetworking;
 
+import java.net.*;
+import java.io.*;
+
 /**
  *
  * @author kito
@@ -20,7 +23,22 @@ public class JavaNetworking {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Welcome to the java networking tutorial.");
+        InetAddress i;
+
+        try {
+           i  = InetAddress.getLocalHost();
+           System.out.println("Host: " + i);
+           System.out.println("--------------------");
+           i = InetAddress.getByName("www.google.com");
+           System.out.println("www.google.com " + i.toString());
+        }
+        catch (UnknownHostException e) {
+            System.out.println("Unknown host exception: " + e.toString());
+        }
         
-    }
-    
+        
+        
+       
+    }  
+
 }
