4682afa92e35d6eae775bf56efc1236dbd3c3851
Kito Joseph <kitojoseph@gmail.com>
2017-08-15 17:52:00 -0400

    Added a test bash script to the mix.

---
+++ redhatcerts/bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#This is a comment in bash
+
+function print_header() {
+echo "Welcome to the bash tutorial."
+date
+}
+
+
+
+print_header #function
+echo "The directories in this folder are listed below."
+ls
