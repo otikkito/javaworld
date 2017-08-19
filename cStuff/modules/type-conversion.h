61799f01e75e850e0befe755aef868523c62e66a
Kito Joseph <kitojoseph@gmail.com>
2017-08-19 09:48:40 -0400

    Added the stub for some of the modules that I will be using.

---
+++ cStuff/modules/type-conversion.h
@@ -0,0 +1,10 @@
+/*type-conversion.h*/
+
+
+/*Include*/
+
+
+/*function prototype*/
+struct binary-digit_t integer_to_binary(int);
+int binary_to_integer(struct binary-digit_t);
+string binary-digit_to_string(struct binary-digit_t);
