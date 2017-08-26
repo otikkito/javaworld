4eac4e52f1bb86c551024fdffe3b383befa50f61
Kito Joseph <kitojoseph@gmail.com>
2017-08-26 09:36:16 -0400

    Updated the .gitignore file and some programs from the book that I am reading.

---
+++ JavaForProgrammers/Chapter2/Welcome1.java
@@ -0,0 +1,11 @@
+//Fig 2.1 Welcome.java
+//Text printing
+
+public class Welcome1{
+	
+	//main method begins execution of Java application
+	public static void main(String[] args){
+			System.out.println("Welcome to Java programming!");
+	}//end method main
+}//end class Welcome1
+	
